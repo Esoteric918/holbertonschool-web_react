8a2ea6395c0de9a66a3f58b3f1da31ce1a3350de
esoteric918 <3411@holbertonstudent.com>
2022-10-06 23:04:16 -0500

    fix import for enyzme shallow

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,26 +1,21 @@
 import { shallow } from 'enzyme';
+import Notifications from './Notifications';
 import React from 'react';
-import NotificationItem from './NotificationItem';
 
-describe('<NotificationItem />', () => {
-  it('renders NotificationIem without crashing', () => {
-    const wrapper = shallow(<NotificationItem />);
-    expect(wrapper.exists()).toBe(true);
-  });
-  it('renders correct html', () => {
-    const wrapper = shallow(<NotificationItem />);
-    expect(wrapper.html()).toContain('<li data-notification-type="default"></li>');
-  });
-  it('renders correct html with type', () => {
-    const wrapper = shallow(<NotificationItem type="urgent" />);
-    expect(wrapper.html()).toContain('<li data-notification-type="urgent"></li>');
-  });
-  it('renders correct html with type and value', () => {
-    const wrapper = shallow(<NotificationItem type="urgent" value="test" />);
-    expect(wrapper.html()).toContain('<li data-notification-type="urgent">test</li>');
-  });
-  it('renders correct html with type and html', () => {
-    const wrapper = shallow(<NotificationItem type="urgent" html={{ __html: '<u>test</u>' }} />);
-    expect(wrapper.html()).toContain('<li data-notification-type="urgent"><u>test</u></li>');
-  });
+
+describe('<Notifications />', () => {
+	it('tests that Notifications renders without crashing', () => {
+		const wrapper = shallow(<Notifications />);
+		expect(wrapper.exists()).toBe(true);
+	})
+
+	it('Checks that the component renders <NotificationItem /> elements', () => {
+		const wrapper = shallow(<Notifications />);
+		expect(wrapper.exists('NotificationItem')).toBe(true);
+	})
+
+	it('Checks first Item renders correct html', () => {
+		const wrapper = shallow(<Notifications />);
+		expect(wrapper.html()).toContain('New course available');
+	})
 });
