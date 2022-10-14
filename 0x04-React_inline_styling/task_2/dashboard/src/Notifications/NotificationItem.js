437044f4e8298f2b5fdb7ee4e8458ff7adf5b0e2
esoteric918 <3411@holbertonstudent.com>
2022-10-13 21:18:44 -0500

    task 2 components updated tests pass

--- 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,24 +1,43 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
+import { StyleSheet, css } from 'aphrodite'
 
-// creat new class NotificationItem that extends React.Component
+const styles = StyleSheet.create({
+  default: {
+    color: 'blue',
+  },
 
-class NotificationItem extends React.Component {
-  render() {
-    const { type, value, html, markAsRead } = this.props;
-    if (html) {
-      return (
-        <li data-notification-type={type} dangerouslySetInnerHTML={html} onClick={markAsRead} />
-      )} else {
-        return (
-         <li data-notification-type={type} onClick={markAsRead}>
-          {value}
-        </li>
-        );
-      }
+  urgent: {
+    color: 'red',
+  },
+});
 
-    }
+const NotificationItem = ({ type, html, value, markAsRead }) => {
+  const itemUrgent = type === 'urgent' ? styles.urgent : styles.default;
+  const itemDefault = type === 'default' ? styles.default : styles.urgent;
+  if (value) {
+    return (
+      <li
+        data-notification-type={type}
+        className={css(itemUrgent)}
+        onClick={markAsRead}
+      >
+        {value}
+      </li>
+    );
+  } else {
+    return (
+      <li
+        className={css(itemDefault)}
+        data-notification-type={type}
+        dangerouslySetInnerHTML={html}
+        onClick={markAsRead}
+      />
+    );
   }
+};
+
+
 
 NotificationItem.propTypes = {
   html: PropTypes.shape({ __html: PropTypes.string }),
