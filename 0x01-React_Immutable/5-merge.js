04e7fd6c5cfb0835bf9e654fed41dc31a72cb4b2
esoteric918 <3411@holbertonstudent.com>
2022-10-19 14:38:19 -0500

    task 5/linter?

--- 0x01-React_Immutable/5-merge.js
+++ 0x01-React_Immutable/5-merge.js
@@ -1,9 +1,9 @@
 import { List, Map } from 'immutable';
 
-function concatElements(page1, page2) {
+export default function concatElements(page1, page2) {
   return List(page1).concat(List(page2));
 }
 
-function mergeElements(page1, page2) {
+export default function mergeElements(page1, page2) {
   return Map(page1).merge(Map(page2));
 }
