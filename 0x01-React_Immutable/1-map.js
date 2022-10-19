be2bd2c96a6b5e7a2d9c5583e3495f48c7f24067
esoteric918 <3411@holbertonstudent.com>
2022-10-19 13:28:35 -0500

    lint?

--- 0x01-React_Immutable/1-map.js
+++ 0x01-React_Immutable/1-map.js
@@ -2,5 +2,5 @@ import { Map } from 'immutable';
 //modify the function getImmutableObject using Map from Immutable.js
 
 export default function getImmutableObject(object) {
-  return Map(object);
+	return Map(object);
 }
