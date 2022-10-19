231e3d9cc917b7e4c48c19e0e66101915670d1e6
esoteric918 <3411@holbertonstudent.com>
2022-10-19 13:23:06 -0500

    task 1

---
+++ 0x01-React_Immutable/1-map.js
@@ -0,0 +1,7 @@
+import { Map } from 'immutable';
+//modify the function getImmutableObject using Map from Immutable.js
+
+export default function getImmutableObject(object) {
+  return Map(object);
+}
+
