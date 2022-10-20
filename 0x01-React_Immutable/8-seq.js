2b5a40e26c3b652722d98f13061699685b4f9e1f
esoteric918 <3411@holbertonstudent.com>
2022-10-19 21:40:33 -0500

    task 8/lint?

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,21 +1,24 @@
 import Immutable from 'immutable';
 
-export default function printBestStudents(students) {
+function printBestStudents(students) {
   const Seggs = Immutable.Seq(students);
   console.log(Seggs);
 
   const bestStudents = Seggs.filter(student => student.score > 70);
-  console.log(bestStudents);
+  // console.log(bestStudents);
 
   const CapStudents = bestStudents.map(students.charAt(0).toUpperCase()) + students.slice(1);
-  console.log(CapStudents);
+  // console.log(CapStudents);
 
   const bestStudentsNames = CapStudents.toJS();
 
-  Object.keys(bestStudentsNames).forEach((key) => {
+  Object.keys(bestStudentsNames).map((key) => {
     const student = bestStudentsNames[key];
     student.firsName = CapStudents(student.firstName);
     student.lastName = CapStudents(student.lastName);
+    return student;
   });
   console.log(bestStudentsNames);
-}
+};
+
+export default printBestStudents;
