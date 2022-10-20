8645ecdf5e1f8b94a039b4fd3ef22d1c6e736e93
esoteric918 <3411@holbertonstudent.com>
2022-10-19 21:55:13 -0500

    run chker to see linter

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,16 +1,17 @@
 import Immutable from 'immutable';
 
+// const Immutable = require('immutable');
+
 function printBestStudents(students) {
   const Seggs = Immutable.Seq(students);
-  console.log(Seggs);
 
   const bestStudents = Seggs.filter(student => student.score > 70);
   // console.log(bestStudents);
 
-  const CapStudents = bestStudents.map(students.charAt(0).toUpperCase()) + students.slice(1);
+  const CapStudents = (str) => str.charAt(0).toUpperCase() + str.slice(1);
   // console.log(CapStudents);
 
-  const bestStudentsNames = CapStudents.toJS();
+  const bestStudentsNames = bestStudents.toJS();
 
   Object.keys(bestStudentsNames).map((key) => {
     const student = bestStudentsNames[key];
@@ -21,4 +22,22 @@ function printBestStudents(students) {
   console.log(bestStudentsNames);
 };
 
-export default printBestStudents;
+// const grades = {
+//   1: {
+//     score: 99,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   },
+//   2: {
+//     score: 59,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   },
+//   3: {
+//     score: 79,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   }
+// };
+
+// printBestStudents(grades);
