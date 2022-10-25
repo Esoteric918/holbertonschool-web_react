0930881f67057a9f1fe4213c2b468f6f17d7708a
esoteric918 <3411@holbertonstudent.com>
2022-10-25 10:40:24 -0500

    fix index on test

--- 0x02-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.test.js
+++ 0x02-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.test.js
@@ -1,13 +1,13 @@
 import getAllNotificationsByUser from './notifications';
 import * as data from '../../../../notifications.json';
 
-// console.log(data.default[0].context, data.default[0].context);
+console.log(data.default[0].context, data.default[2].context);
 
 describe('getAllNotificationsByUser', () => {
   it('Verifies that getAllNotificationsByUser returns correct data using arrayContaining', () => {
     expect(getAllNotificationsByUser("5debd764a7c57c7839d722e9")).toEqual(
       expect.arrayContaining([
-        expect.objectContaining(data.default[0].context, data.default[1].context)
+        expect.objectContaining(data.default[0].context, data.default[2].context)
       ])
     );
   })
